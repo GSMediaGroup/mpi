5ab10c32d773e281e23b1c8d00751a23ecee4b08
Grish <grisha@myperfectidea.net>
2019-09-06 19:44:13 +0400

    Simple Change Live V1.3.6

--- php/sendmail/sendmail.php
+++ php/sendmail/sendmail.php
@@ -130,6 +130,11 @@ class SendMail
 }
 
 try {
+    if ('POST' != $_SERVER['REQUEST_METHOD']) {
+        header('location: /');
+        exit;
+    }
+
     // Allow from any origin
     if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
